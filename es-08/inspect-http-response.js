f10e4abf960790770ca00cb90766efcd16f4df48
Camilla Devasini <camilladevasini@gmail.com>
2022-11-26 12:49:13 +0100

    add es-08 Inspect an HTTP response with curl

---
+++ es-08/inspect-http-response.js
@@ -0,0 +1,2 @@
+// nel terinale test tramite: curl https://jsonplaceholder.typicode.com/posts/1/comments -v
+// il valore del reponse header content-type all'interno dell'Http response è: application/json; charset=utf-8
