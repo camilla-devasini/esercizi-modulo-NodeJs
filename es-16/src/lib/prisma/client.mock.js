4f42fbc02dda9348d37f70eaf758664393e204e9
Camilla Devasini <camilladevasini@gmail.com>
2022-12-12 13:12:39 +0100

    correzione configurazioni

--- es-16/src/lib/prisma/client.mock.js
+++ es-16/src/lib/prisma/client.mock.js
@@ -1,8 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prismaMock = void 0;
 const jest_mock_extended_1 = require("jest-mock-extended");
-const client_1 = require("./client");
+const client_1 = __importDefault(require("./client"));
 jest.mock("./client", () => ({
     __esModule: true,
     default: (0, jest_mock_extended_1.mockDeep)(),
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  verbose: true,
+  clearMocks: true,
+  setupFilesAfterEnv: ["./src/lib/prisma/client.mock.ts"],
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
